06cbbfeffe88bc5f8d51c20f4ec49c93df7826c6
n2God <kardasz.lukasz@gmail.com>
2019-12-23 19:34:56 +0100

    add ConnectionProvider to local database

---
+++ src/pl/n2god/dao/util/ConnectionProvider.java
@@ -0,0 +1,34 @@
+package pl.n2god.dao.util;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * @author n2god on 23/12/2019
+ * @project dao
+ */
+public class ConnectionProvider {
+	private static DataSource dataSource;
+
+	public static Connection getConnection() throws SQLException {
+		return getDSInstance().getConnection();
+	}
+
+	private static DataSource getDSInstance() {
+		if (dataSource == null){
+			try {
+				Context context = new InitialContext();
+				Context envContext = (Context) context.lookup("java:comp/env");
+				dataSource = (DataSource) envContext.lookup("jdbc/library");
+			} catch (NamingException e) {
+				e.printStackTrace();
+			}
+		}
+		return dataSource;
+	}
+}
